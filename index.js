bf2219cbee3f3d93769c2db7d22f9410905b89f2
Dalton Ricker <dricker@wayfair.com>
2021-01-07 18:06:58 -0500

    renamed the original to reflect it as such, created a new file in which to begin the refactor, extracted the switch statement into its own function and changed the name of one of the variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const plays = require("./data/plays");
 const invoices = require("./data/invoices");
-const statement = require("./statement");
+// const statement = require("./original");
+const statement = require("./refactor");
 
 const result = statement.statement(invoices, plays);
 console.log(result);
