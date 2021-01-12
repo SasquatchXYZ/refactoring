a9915d5bfacc6f9372e0be1f8356a8690e024dff
Dalton Ricker <dricker@wayfair.com>
2021-01-12 10:27:12 -0500

    splitting the phases of calculation and formatting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const plays = require("./data/plays");
 const invoices = require("./data/invoices");
 // const statement = require("./original");
-const statement = require("./refactor");
+// const statement = require("./refactor");
+const statement = require("./split")
 
 const result = statement.statement(invoices, plays);
 console.log(result);
