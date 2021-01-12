9e36dcd34d7e29a5b698cc08bd2ede30c056958d
Dalton Ricker <dricker@wayfair.com>
2021-01-12 18:11:24 -0500

    added in the html statement

--- split.js
+++ split.js
@@ -2,7 +2,7 @@
 const createStatementData = require("./createStatementData");
 
 function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays))
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
@@ -15,6 +15,27 @@ function renderPlainText(data) {
 
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+    for (let perf of data.performances) {
+        // print line for this order
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
     return result;
 }
 
