66384b0acdd93901405b255facbeae79b3eae6dd
Dalton Ricker <dricker@wayfair.com>
2021-01-13 13:03:53 -0500

    making the performance calculator polymorphic

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,7 @@
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
+}
+
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
@@ -44,7 +48,7 @@ module.exports = function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
