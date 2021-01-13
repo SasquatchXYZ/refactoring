76e81e07a40a0f97e696843a12b97bce94549d02
Dalton Ricker <dricker@wayfair.com>
2021-01-13 13:16:30 -0500

    reorganize the structure for cleanliness

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,34 @@
+// export default function createStatementData(invoice, plays) {
+module.exports = function createStatementData(invoice, plays) {
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function totalAmount(data) {
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    }
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
     switch (aPlay.type) {
         case "tragedy":
@@ -46,44 +77,5 @@ class ComedyCalculator extends PerformanceCalculator {
 
     get volumeCredits() {
         return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
-}
-
-// export default function createStatementData(invoice, plays) {
-module.exports = function createStatementData(invoice, plays) {
-    const result = {};
-    result.customer = invoice.customer;
-    result.performances = invoice.performances.map(enrichPerformance);
-    result.totalAmount = totalAmount(result);
-    result.totalVolumeCredits = totalVolumeCredits(result);
-    return result;
-
-    function enrichPerformance(aPerformance) {
-        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
-        const result = Object.assign({}, aPerformance);
-        result.play = calculator.play;
-        result.amount = calculator.amount;
-        result.volumeCredits = calculator.volumeCredits;
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
